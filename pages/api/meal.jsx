6ee7ce65cc3fe3b79022696aa839d3cdbe887971
bonobo0 <>
2022-06-02 23:33:30 +0900

    add: allergie info

--- pages/api/meal.jsx
+++ pages/api/meal.jsx
@@ -1,3 +1,24 @@
+const allergieKorean = {
+    1: "난류",
+    2: "우유",
+    3: "메밀",
+    4: "땅콩",
+    5: "대두",
+    6: "밀",
+    7: "고등어",
+    8: "게",
+    9: "새우",
+    10: "돼지고기",
+    11: "복숭아",
+    12: "토마토",
+    13: "아황산류",
+    14: "호두",
+    15: "닭고기",
+    16: "쇠고기",
+    17: "오징어",
+    18: "조개류",
+
+}
 export default async function getServerSideProps(req, res) {
     const schoolname = req.query.schoolname;
     const inputDate = req.query.date;
@@ -9,12 +30,12 @@ export default async function getServerSideProps(req, res) {
     const schoolInfo = await getSchoolInfo();
     const schoolID = schoolInfo.schoolInfo[1].row[0].SD_SCHUL_CODE;
     const officeID = schoolInfo.schoolInfo[1].row[0].ATPT_OFCDC_SC_CODE;
+    let currentDate = 0;
     let type = req.query.type;
     if (type == undefined) {
         type = 2;
     }
     async function getMeal() {
-        let currentDate = 0
         if (inputDate == undefined) {
             const date = new Date();
             const utc = date.getTime() + (date.getTimezoneOffset() * 60000);
@@ -42,20 +63,49 @@ export default async function getServerSideProps(req, res) {
     
     const meal = await getMeal();
     if (meal.mealServiceDietInfo == undefined) {
-        return res.json({menu: 'No Data', calorie:'No Data', nutrient:'No Data'});
+        return res.json({date: currentDate, menu: 'No Data', calorie:'No Data', nutrient:'No Data', allergie: 'No Data'});
     }
     const data = meal.mealServiceDietInfo[1].row;
     for (let i = 0; i < data.length; i++) {
             if (data[i]['MMEAL_SC_CODE'] == type) {
+                const allergieRegex = new RegExp(/\([^)]*\)/g);
+                let allergies = data[i]['DDISH_NM'].match(allergieRegex);
+                if (allergies != null) {
+                    allergies = allergies.join('');
+                    allergies = allergies.replace(/[\(\)\.]/g, ' ');
+                    allergies = allergies.split(' ');
+                    const set = new Set(allergies);
+                    allergies = [...set];
+                    allergies.splice("", 1);
+                    console.log(allergies)
+                    for (let j = 0; j < allergies.length; j++) {
+                        if (allergieKorean[allergies[j]] != null)
+                        {
+                            allergies[j] = allergieKorean[allergies[j]];
+                        }
+                        else {
+                            allergies.splice(j, 1);
+                        }
+                    }
+                    if (!isNaN(allergies[0])) {
+                        allergies[0] = allergieKorean[allergies[0]];
+                    }
+                }
                 return res.json({
+                    date: currentDate,
                     menu: data[i]['DDISH_NM'].replace(/^\s+|<br\/>/g, '').replace(/\([^)]*\)/g, "").replaceAll('*', ''),
                     calorie: data[i]['CAL_INFO'],
                     nutrient: data[i]['NTR_INFO'].replaceAll('<br/>', ' '),
+                    allergie: allergies
                 });
             }
     };
     return res.json({
-        meal: null
+        date: currentDate,
+        meal: null,
+        calorie: null,
+        nutrient: null,
+        allergie: null
     })
     
 
